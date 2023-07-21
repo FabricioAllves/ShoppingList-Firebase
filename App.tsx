1107b9de9158dd5e48d07027bca449cb08b2a9fe
FabricioAllves <fabricioohh@gmail.com>
2023-07-21 17:50:50 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+
+import { Routes } from './src/routes';
+import theme from './src/theme';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar style="light" />
+      <Routes />
+    </ThemeProvider>
+  );
+}
